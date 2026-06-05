b9027b3b9bebe9cbfba8d0e2ad0374af86ed54a6
Allison Arcand <arcanda@wit.edu>
2026-06-05 13:05:00 -0400

    Added add event section- not fully implemented

--- CalendarAssistant/src/App.jsx
+++ CalendarAssistant/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./login";
 import MainPage from "./main";
+import AddEventPage from "./addEvent";
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/main" element={<MainPage />} />
+        <Route path="/addEvent" element={<AddEventPage />} />
       </Routes>
   );
 }
