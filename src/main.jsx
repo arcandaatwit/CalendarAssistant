e4637463f81291e425df129083b517ce6625c29d
Allison Arcand <arcanda@wit.edu>
2026-06-14 23:38:13 -0400

    Adjusting layout across all pages and adjusting container

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import './index.css';
-
+import Layout from "./Layout";
 
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import { format, parse, startOfWeek, getDay } from "date-fns";
